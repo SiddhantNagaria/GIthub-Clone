5302c334fe2a124212c5f147d6199422fa7d20bf
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-05-14 11:13:25 +0530

    init logic

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,5 +1,18 @@
+const fs = require('fs').promises;
+const path = require('path');
+
 async function initRepo() {
-    console.log('Init command called');
+    const repoPath = path.resolve(process.cwd(), '.mygit');
+    const commitsPath = path.join(repoPath, 'commits');
+
+    try{
+        await fs.mkdir(repoPath, { recursive: true });
+        await fs.mkdir(commitsPath, { recursive: true });
+        await fs.writeFile(path.join(repoPath, "config.json"), JSON.stringify({ bucket: process.env.S3_BUCKET }));
+        console.log('repository initialized');
+    }catch(err){
+        console.error("Error creating repository:", err);
+    }
 }
 
 module.exports = { initRepo };
