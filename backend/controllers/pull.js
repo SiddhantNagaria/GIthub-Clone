ba90cadfe1ccab4a95809eb14c34e1af3328564c
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-05-16 21:11:08 +0530

    pull logic

--- backend/controllers/pull.js
+++ backend/controllers/pull.js
@@ -1,5 +1,31 @@
-async function pullRepo(){
-    console.log("pull function called");
+const fs = require("fs").promises;
+const path = require("path");
+const { s3, S3_BUCKET } = require("../config/aws-config");
+
+
+async function pullRepo() {
+    const repoPath = path.resolve(process.cwd(), ".mygit");
+    const commitsPath = path.join(repoPath, "commits");
+
+    try {
+        const data = await s3.listObjectsV2({ Bucket: S3_BUCKET, Prefix: "commits/" }).promise(); //list objects
+        const objects = data.Contents;
+
+        for (const object of objects) {
+            const key = object.Key;
+            const commitDir = path.join(commitsPath, path.dirname(key).split("/").pop());
+            await fs.mkdir(commitDir, { recursive: true }); //create commit directory if it doesn't exist
+            const params = {
+                Bucket: S3_BUCKET,
+                Key: key
+            };
+            const fileContent = await s3.getObject(params).promise(); //get object from S3
+            await fs.writeFile(path.join(repoPath, key),fileContent.Body);
+            console.log("All commits pulled successfully from S3");
+        }
+    } catch (err) {
+        console.error("Error pulling repo:", err);
+    }
 }
 
 module.exports = { pullRepo };
