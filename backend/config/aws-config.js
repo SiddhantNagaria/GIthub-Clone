47af729dce45bb71423afcbbf001ca7ba8227a62
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-05-16 14:41:35 +0530

    AWS config setup

---
+++ backend/config/aws-config.js
@@ -0,0 +1,9 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({region:'eu-north-1'});
+
+const s3 = new AWS.S3();
+
+const S3_BUCKET = 'siddhantgithubbucket';
+
+module.exports = {s3, S3_BUCKET};
