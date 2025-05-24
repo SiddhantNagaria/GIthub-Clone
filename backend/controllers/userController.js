92e88a61008f86e34278086401304a2406af0741
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-05-25 01:08:35 +0530

    updating and deleting user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const { MongoClient } = require("mongodb");
+const { MongoClient, returnDocument } = require("mongodb");
 const dotenv = require("dotenv");
 var ObjectId = require("mongodb").ObjectId;
 
@@ -110,7 +110,7 @@ async function getUserProfile(req, res) {
         if (!user) {
             return res.status(404).json({ message: "User not found" });
         }
-        res.send(user, {message:"Profile fetched !"})
+        res.send(user, { message: "Profile fetched !" })
     } catch (err) {
         console.error("Error during fetching:", err.message);
         res.status(500).send("server error !");
@@ -118,11 +118,63 @@ async function getUserProfile(req, res) {
 };
 
 async function updateUserProfile(req, res) {
-    res.send("profile updated");
-};
+    const currentId = req.params.id;
+    const { email, password } = req.body;
+
+    try {
+        await connectClient();
+        const db = client.db("githubclone");
+        const usersCollection = db.collection("users");
+
+        let updateFields = {};
+        if (email) updateFields.email = email;
+        if (password) {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            updateFields.password = hashedPassword;
+        }
+
+        const updatedUser = await usersCollection.findOneAndUpdate(
+            { _id: new ObjectId(currentId) },
+            { $set: updateFields },
+            { returnDocument: "after" }
+        );
+        if (!updatedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        return res.status(200).json({
+            message: "Profile updated successfully",
+            user: updatedUser,
+        });
+
+    } catch (err) {
+        console.error("Error during update:", err.message);
+        return res.status(500).send("Server error!");
+    }
+}
+
+
 
 async function deleteUserProfile(req, res) {
-    res.send("profile deleted");
+    const currentId = req.params.id;
+
+    try {
+        await connectClient();
+        const db = client.db("githubclone");
+        const usersCollection = db.collection("users");
+
+        const result = await usersCollection.deleteOne({
+            _id: new ObjectId(currentId),
+        })
+        if (result.deleteCount == 0) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json({ message: "User Profile deleted" });
+    } catch (err) {
+        console.error("Error during fetching:", err.message);
+        res.status(500).send("server error !");
+    }
 };
 
 module.exports = {
