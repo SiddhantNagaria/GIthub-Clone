c6959929059124566b9d055c2af102b6f544f1b3
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-05-25 23:44:48 +0530

    fetch repository details

--- backend/controllers/repoController.js
+++ backend/controllers/repoController.js
@@ -13,7 +13,7 @@ async function createRpository(req, res) {
         if (!mongoose.Types.ObjectId.isValid(owner)) {
             return res.status(400).json({ error: "Invalid User Id" });
         }
-        
+
         const newRepository = new Repository({
             name, description, visibility, content, owner, issues
         });
@@ -21,8 +21,8 @@ async function createRpository(req, res) {
         const result = await newRepository.save();
 
         res.status(201).json({
-            message:"Repository Created",
-            repositoryId : result._id,
+            message: "Repository Created",
+            repositoryId: result._id,
         });
     } catch (err) {
         console.error("Error during repository creation:", err.message);
@@ -31,15 +31,40 @@ async function createRpository(req, res) {
 };
 
 async function getALlRepositories(req, res) {
-    res.send("All repositories fetched");
+    try {
+        const repositories = await Repository.find({}).populate("owner")
+            .populate("issues");
+        res.json(repositories);
+    } catch (err) {
+        console.error("Error during repository creation:", err.message);
+        res.status(500).send("server error !");
+    }
 };
 
 async function fetchRespositoryById(req, res) {
-    res.send("Repository details fetched");
+    const { id } = req.params;
+    try {
+        const repository = await Repository.find({ _id: id })
+            .populate("owner")
+            .populate("issues");
+        res.json(repository);
+    } catch (err) {
+        console.error("Error during repository fetching:", err.message);
+        res.status(500).send("server error !");
+    }
 };
 
 async function fetchRespositoryByName(req, res) {
-    res.send("Repository details fetched");
+    const { name } = req.params;
+    try {
+        const repository = await Repository.find({ name })
+            .populate("owner")
+            .populate("issues");
+        res.json(repository);
+    } catch (err) {
+        console.error("Error during repository fetching:", err.message);
+        res.status(500).send("server error !");
+    }
 };
 
 async function fetchRespositoryForCurrentUser(req, res) {
