5cdefa30c00720cd3732acc84fd9a1db72f56fd7
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-05-26 00:39:12 +0530

    issue controller functionality

--- backend/controllers/issueController.js
+++ backend/controllers/issueController.js
@@ -1,21 +1,87 @@
-const createIssue = (req, res) => {
-    res.send("issue created");
+const mongoose = require('mongoose');
+const Repository = require('../models/repoModel');
+const User = require('../models/userModel');
+const Issue = require('../models/issueModel');
+
+async function createIssue(req, res) {
+    const { title, description } = req.body;
+    const { id } = req.params;
+    try {
+        const issue = new Issue({
+            title,
+            description,
+            repository: id,
+        });
+        await issue.save();
+        res.status(201).json(issue);
+    } catch (err) {
+        console.error("Error during issue creation:", err.message);
+        res.status(500).send("server error !");
+    }
+
 };
 
-const updateIssueById = (req, res) => {
-    res.send("issue updated");
+async function updateIssueById(req, res) {
+    const { id } = req.params;
+    const { title, description, status } = req.body;
+    try {
+        const issue = await Issue.findById(id);
+        if (!issue) {
+            return res.status(404).json({ error: "issue not found" });
+        }
+        issue.title = title;
+        issue.description = description;
+        issue.status = status;
+
+        await issue.save();
+        res.json(issue, { message: "issue updated successfully" });
+    } catch (err) {
+        console.error("Error during issue updation:", err.message);
+        res.status(500).send("server error !");
+    }
 };
 
-const deleteIssueById = (req, res) => {
-    res.send("Issue updated");
+async function deleteIssueById(req, res) {
+    const { id } = req.params;
+    try {
+        const issue = Issue.findByIdAndDelete(id);
+
+        if (!issue) {
+            return res.status(404).json({ error: "issue not found" });
+        }
+    } catch (err) {
+        console.error("Error during issue deletion:", err.message);
+        res.status(500).send("server error !");
+    }
 };
 
-const getAllIssues = (req, res) => {
-    res.send("All issues fetched");
+async function getAllIssues(req, res) {
+    const { id } = req.params;
+    try {
+        const issues = Issue.find({ repository: id });
+
+        if (!issue) {
+            return res.status(404).json({ error: "issue not found" });
+        }
+        res.status(200).json(issues);
+    } catch (err) {
+        console.error("Error during issue fetching:", err.message);
+        res.status(500).send("server error !");
+    }
 };
 
-const getIssueById = (req, res) => {
-    res.send("Issue Details Fetched");
+async function getIssueById(req, res) {
+    const { id } = req.params;
+    try {
+        const issue = await Issue.findById(id);
+        if (!issue) {
+            return res.status(404).json({ error: "issue not found" });
+        }
+        res.json(issue);
+    } catch (err) {
+        console.error("Error during issue updation:", err.message);
+        res.status(500).send("server error !");
+    }
 };
 
 module.exports = {
